05c316a9947e737920e690b0a846df56e9a17127
JohnTWD <john.theworlddestroyer@gmail.com>
2023-11-17 21:51:23 +0800

    Damn, AI really will replace humans, time to kms

--- src/main/java/anticope/rejects/mixin/PlayerEntityMixin.java
+++ src/main/java/anticope/rejects/mixin/PlayerEntityMixin.java
@@ -1,8 +1,13 @@
 package anticope.rejects.mixin;
 
 import anticope.rejects.events.OffGroundSpeedEvent;
+import anticope.rejects.modules.MacroAnchorAuto;
+import anticope.rejects.modules.ManualCrystal;
 import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,5 +18,18 @@ public class PlayerEntityMixin {
     @Inject(method = "getOffGroundSpeed", at = @At("RETURN"), cancellable = true)
     private void onGetOffGroundSpeed(CallbackInfoReturnable<Float> cir) {
         cir.setReturnValue(MeteorClient.EVENT_BUS.post(OffGroundSpeedEvent.get(cir.getReturnValueF())).speed);
+    }
+
+    @Inject(
+            method = "eatFood",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void onEatFood(World world, ItemStack stack, CallbackInfoReturnable<ItemStack> cir) {
+        // Check if the right-click button is held down
+        if (Modules.get().get(ManualCrystal.class).shouldStopItemUse()) {
+            // Cancel the event to prevent eating
+            cir.setReturnValue(stack);
+        }
     }
 }
