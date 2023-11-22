799e8bf1cb8474e95b3f3836e187b0019e4d5125
JohnTWD <john.theworlddestroyer@gmail.com>
2023-11-23 05:52:48 +0800

    Ported old autoez

--- src/main/java/anticope/rejects/MeteorRejectsAddon.java
+++ src/main/java/anticope/rejects/MeteorRejectsAddon.java
@@ -30,7 +30,7 @@ public class MeteorRejectsAddon extends MeteorAddon {
         // Modules
         Modules modules = Modules.get();
 
-        modules.add(new Announcer());
+        modules.add(new AutoEz());
         modules.add(new PacketLogger());
         modules.add(new ManualCrystal());
         modules.add(new TweakedAutoTool());
